98362ccc1c9b3ba67e2e330cfb78edc0d69f4b64
Jason Leonhard <leonhard.jason@gmail.com>
2020-12-17 13:23:21 -0800

    add a read_lines_example method

--- src/lib.rs
+++ src/lib.rs
@@ -15,4 +15,21 @@ pub mod file_io {
         let file = File::open(filename)?;
         Ok(io::BufReader::new(file).lines())
     }
+    pub fn read_lines_example() {
+        println!(
+            "{}",
+            "
+use leonhard_lib::file_io;
+
+let mut rows: Vec<String> = Vec::new();
+if let Ok(lines) = file_io::read_lines('./data/day3.txt') {
+    for line in lines {
+        if let Ok(raw_line) = line {
+            rows.push(raw_line.clone());
+        }
+    }
+}
+            "
+        );
+    }
 }
